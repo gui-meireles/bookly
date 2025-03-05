a3fc6d6f80e5a5ecae7bdf8594ba79be822db50e
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2025-03-05 09:23:59 -0300

    feat: Implementing authentication with jwt and endpoints of create and login user

---
+++ src/auth/schemas.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import uuid
+
+from pydantic import BaseModel, Field
+
+
+class UserCreateModel(BaseModel):
+    first_name: str = Field(max_length=25)
+    last_name: str = Field(max_length=25)
+    username: str = Field(max_length=12)
+    email: str = Field(max_length=100)
+    password: str = Field(min_length=6)
+
+
+class UserModel(BaseModel):
+    uid: uuid.UUID
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool
+    password_hash: str = Field(exclude=True)
+    created_at: datetime
+    updated_at: datetime
+
+
+class UserLoginModel(BaseModel):
+    email: str = Field(max_length=100)
+    password: str = Field(min_length=6)
